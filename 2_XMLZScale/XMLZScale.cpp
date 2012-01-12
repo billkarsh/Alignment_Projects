14d08ef0b748439624fc96ebb80cbd70271babe2
billKarsh <karshb@janelia.hhmi.org>
2012-01-12 18:48:18 -0500

    Add XMLZScale tool

---
+++ 2_XMLZScale/XMLZScale.cpp
@@ -0,0 +1,207 @@
+//
+// Multiply each layer's z tag by given -mul= factor.
+//
+
+#include	"Cmdline.h"
+#include	"File.h"
+#include	"TrakEM2_UTL.h"
+
+
+/* --------------------------------------------------------------- */
+/* Macros -------------------------------------------------------- */
+/* --------------------------------------------------------------- */
+
+/* --------------------------------------------------------------- */
+/* Types --------------------------------------------------------- */
+/* --------------------------------------------------------------- */
+
+/* --------------------------------------------------------------- */
+/* CArgs_xml ----------------------------------------------------- */
+/* --------------------------------------------------------------- */
+
+class CArgs_xml {
+
+public:
+	double	mul, zmin, zmax;
+	char	*infile;
+
+public:
+	CArgs_xml()
+	{
+		mul		= 1.0;
+		zmin	= 0.0;
+		zmax	= 32768.0;
+		infile	= NULL;
+	};
+
+	void SetCmdLine( int argc, char* argv[] );
+};
+
+/* --------------------------------------------------------------- */
+/* Statics ------------------------------------------------------- */
+/* --------------------------------------------------------------- */
+
+static CArgs_xml	gArgs;
+static FILE*		flog = NULL;
+
+
+
+
+
+
+/* --------------------------------------------------------------- */
+/* SetCmdLine ---------------------------------------------------- */
+/* --------------------------------------------------------------- */
+
+void CArgs_xml::SetCmdLine( int argc, char* argv[] )
+{
+// start log
+
+	flog = FileOpenOrDie( "XMLZScale.log", "w" );
+
+// log start time
+
+	time_t	t0 = time( NULL );
+	char	atime[32];
+
+	strcpy( atime, ctime( &t0 ) );
+	atime[24] = '\0';	// remove the newline
+
+	fprintf( flog, "Start: %s ", atime );
+
+// parse command line args
+
+	if( argc < 5 ) {
+		printf( "Usage: XMLZScale <xml-file1> -mul=f -zmin=i -zmax=j.\n" );
+		exit( 42 );
+	}
+
+	for( int i = 1; i < argc; ++i ) {
+
+		// echo to log
+		fprintf( flog, "%s ", argv[i] );
+
+		if( argv[i][0] != '-' )
+			infile = argv[i];
+		else if( GetArg( &mul, "-mul=%lf", argv[i] ) )
+			;
+		else if( GetArg( &zmin, "-zmin=%lf", argv[i] ) )
+			;
+		else if( GetArg( &zmax, "-zmax=%lf", argv[i] ) )
+			;
+		else {
+			printf( "Did not understand option [%s].\n", argv[i] );
+			exit( 42 );
+		}
+	}
+
+	fprintf( flog, "\n\n" );
+	fflush( flog );
+}
+
+/* --------------------------------------------------------------- */
+/* Process ------------------------------------------------------- */
+/* --------------------------------------------------------------- */
+
+static void Process()
+{
+/* ------------- */
+/* Load document */
+/* ------------- */
+
+	TiXmlDocument	doc( gArgs.infile );
+
+	if( !doc.LoadFile() ) {
+		fprintf( flog,
+		"Could not open XML file [%s].\n", gArgs.infile );
+		exit( 42 );
+	}
+
+/* ---------------- */
+/* Verify <trakem2> */
+/* ---------------- */
+
+	TiXmlHandle		hdoc( &doc );
+	TiXmlElement	*layer;
+
+	if( !doc.FirstChild() ) {
+		fprintf( flog, "No trakEM2 node [%s].\n", gArgs.infile );
+		exit( 42 );
+	}
+
+	layer = hdoc.FirstChild( "trakem2" )
+				.FirstChild( "t2_layer_set" )
+				.FirstChild( "t2_layer" )
+				.ToElement();
+
+	if( !layer ) {
+		fprintf( flog, "No t2_layer [%s].\n", gArgs.infile );
+		exit( 42 );
+	}
+
+/* -------------- */
+/* For each layer */
+/* -------------- */
+
+	for( ; layer; layer = layer->NextSiblingElement() ) {
+
+		/* ----------------- */
+		/* Layer-level stuff */
+		/* ----------------- */
+
+		double	z = atof( layer->Attribute( "z" ) );
+
+		if( z > gArgs.zmax )
+			break;
+
+		if( z < gArgs.zmin )
+			continue;
+
+		z *= gArgs.mul;
+
+		layer->SetDoubleAttribute( "z", z );
+	}
+
+/* ---- */
+/* Save */
+/* ---- */
+
+	doc.SaveFile( "xmltmp.txt" );
+
+/* ----------------- */
+/* Copy !DOCTYPE tag */
+/* ----------------- */
+
+	CopyDTD( gArgs.infile, "xmltmp.txt" );
+}
+
+/* --------------------------------------------------------------- */
+/* main ---------------------------------------------------------- */
+/* --------------------------------------------------------------- */
+
+int main( int argc, char* argv[] )
+{
+/* ------------------ */
+/* Parse command line */
+/* ------------------ */
+
+	gArgs.SetCmdLine( argc, argv );
+
+/* ------------- */
+/* Write new xml */
+/* ------------- */
+
+	Process();
+
+/* ---- */
+/* Done */
+/* ---- */
+
+exit:
+	fprintf( flog, "\n" );
+	fclose( flog );
+
+	return 0;
+}
+
+
