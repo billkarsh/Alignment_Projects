6fc02c377ac210a5d67694843fc6bad89b5cec6f
billKarsh <karshb@janelia.hhmi.org>
2012-06-04 12:35:03 -0400

    Retire thumbs parabolic PeakHunt

--- 1_Thumbs/Thumbs.cpp
+++ 1_Thumbs/Thumbs.cpp
@@ -402,12 +402,12 @@ static void RotatePoints(
 {
 	double	c	= cos( theta ) * GBL.ctx.SCALE,
 			s	= sin( theta ) * GBL.ctx.SCALE;
-	TForm	ao( GBL.ctx.XSCALE * c, -GBL.ctx.YSCALE * s, 0.0,
+	TForm	Ta( GBL.ctx.XSCALE * c, -GBL.ctx.YSCALE * s, 0.0,
 				GBL.ctx.XSCALE * s,  GBL.ctx.YSCALE * c, 0.0 );
 	TForm	T0;
 
 	MultiplyTrans( T0, Tskew, Tptwk );
-	MultiplyTrans( T, ao, T0 );
+	MultiplyTrans( T, Ta, T0 );
 	T.Apply_R_Part( pts );
 }
 
@@ -933,75 +933,6 @@ static double AngleScanWithTweaks(
 /* --------------------------------------------------------------- */
 /* PeakHunt ------------------------------------------------------ */
 /* --------------------------------------------------------------- */
-
-#if 0
-// Repeated parabola fits for peak on a narrowing angle range.
-//
-static double PeakHunt(
-	CorRec	&best,
-	double	hlfwid,
-	ThmRec	&thm,
-	FILE*	flog )
-{
-	CorRec	B0	= best;
-	double	L	= best.A - hlfwid,
-			R	= best.A + hlfwid;
-	int		k	= 0;
-
-	clock_t	t0 = StartTiming();
-
-	while( R - L > 0.0001 ) {
-
-		CorRec	C;
-		double	x1, y0, y2;
-
-		++k;
-
-		// left
-		RFromAngle( C, L, thm, flog );
-		y0 = C.R;
-
-		// right
-		RFromAngle( C, R, thm, flog );
-		y2 = C.R;
-
-		// middle
-		RFromAngle( C, x1 = (L+R)/2.0, thm, flog );
-
-		// estimate peak position
-		C.A = NewXFromParabola( x1, (R-L)/2.0, y0, C.R, y2 );
-
-		// sanity check
-		if( C.A <= L || C.A >= R )
-			break;
-
-		// compete estimate against best so far
-		RFromAngle( C, C.A, thm, flog );
-
-		if( C.R > best.R )
-			best = C;
-
-		//fprintf( flog,
-		//"*** [%f,%f] [%f,%f] [%f,%f] <newx=%f>.\n",
-		//L,y0, best.A,best.R, R,y2, x1 );
-
-		x1	= fmin( best.A - L, R - best.A ) / 4.0;
-		L	= best.A - x1;
-		R	= best.A + x1;
-	}
-
-	if( B0.R > best.R )
-		best = B0;
-
-	fprintf( flog,
-	"PeakHunt: Best: K=%d, R=%.3f, A=%.3f, X=%.3f, Y=%.3f\n",
-	k, best.R, best.A, best.X, best.Y );
-
-	StopTiming( stdout, "PeakHunt", t0 );
-
-	return best.R;
-}
-#endif
 
 // Bracket search for peak.
 //
