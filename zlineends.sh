361949417d631595afa4365bb5838ddc42a477bf
billKarsh <karshb@janelia.hhmi.org>
2012-07-13 20:52:47 -0400

    Update zlineends to POSIX

--- zlineends.sh
+++ zlineends.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 
 # > ./zlineends -> List all files with <cr><lf> ends
 # > ./zlineends -f  dir -> Make Unix ends in dir
 
 
-if [ "$1" = "-f" ]; then
-	for ifile in `grep -rl $'\r' $2*`;
+if [ "$1" = "-f" ]
+then
+	for ifile in `grep -rl $'\r' $2*`
 	do
 		sed -i 's|\r||g' $ifile
 	done
