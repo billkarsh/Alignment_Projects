eebaf4f0ad7e352bb9e2598053615f45931d2a19
billKarsh <karshb@janelia.hhmi.org>
2012-11-26 17:34:46 -0500

    Improve Tab rotation

--- 1_DMesh/CThmUtil.cpp
+++ 1_DMesh/CThmUtil.cpp
@@ -42,6 +42,31 @@ bool CThmUtil::FromLog( vector<TForm> &guesses )
 }
 
 /* --------------------------------------------------------------- */
+/* RotateTab ----------------------------------------------------- */
+/* --------------------------------------------------------------- */
+
+// Rotate Tab to reflect starting ang0 (degrees).
+//
+static void RotateTab( TForm &Tab, double ang0 )
+{
+	double	acur = RadiansFromAffine( Tab );
+
+	ang0 *= PI/180.0;
+
+	if( acur != ang0 ) {
+
+		TForm	R;
+		double	Vx = Tab.t[2],
+				Vy = Tab.t[5];
+
+		R.NUSetRot( ang0 - acur );
+		Tab.SetXY( 0, 0 );
+		MultiplyTrans( Tab, R, TForm( Tab ) );
+		Tab.SetXY( Vx, Vy );
+	}
+}
+
+/* --------------------------------------------------------------- */
 /* SetStartingAngle ---------------------------------------------- */
 /* --------------------------------------------------------------- */
 
@@ -54,8 +79,7 @@ bool CThmUtil::FromLog( vector<TForm> &guesses )
 //
 // All modes use ang0 as the dbgCor angle and the center angle.
 // In all cases, Tab, the starting guess for the sought rigid
-// transform, is adjusted so that Tab, written as R+V, becomes
-// R(ang0)+V.
+// transform, is adjusted to reflect ang0.
 //
 // Generally, Tab is used from the idb, unless it was overridden
 // from the command line as -Tab=. Further, if the command line
@@ -70,8 +94,6 @@ bool CThmUtil::FromLog( vector<TForm> &guesses )
 //
 int CThmUtil::SetStartingAngle( double CTR )
 {
-	double			Vx = Tab.t[2],
-					Vy = Tab.t[5];
 	vector<ThmPair>	tpr;
 	int				ntpr, nprior = 0;
 
@@ -81,8 +103,7 @@ int CThmUtil::SetStartingAngle( double CTR )
 
 		if( CTR != 999.0 ) {
 			ang0 = CTR;
-			Tab.NUSetRot( CTR * PI/180.0 );
-			Tab.SetXY( Vx, Vy );
+			RotateTab( Tab, CTR );
 		}
 		else
 			ang0 = 180.0/PI * RadiansFromAffine( Tab );
@@ -125,10 +146,9 @@ int CThmUtil::SetStartingAngle( double CTR )
 	if( fabs( ang0 ) < 0.001 )
 		ang0 = 0.0;
 
-// Tab becomes R(ang0) + V
+// Adjust Tab
 
-	Tab.NUSetRot( ang0 * PI/180.0 );
-	Tab.SetXY( Vx, Vy );
+	RotateTab( Tab, ang0 );
 
 // Adjust OLAP2D_XL
 
