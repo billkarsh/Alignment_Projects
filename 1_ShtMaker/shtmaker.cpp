ebeeacc77027c07c1100d1c5e19858dc60de7ec8
billKarsh <karshb@janelia.hhmi.org>
2013-06-18 16:46:25 -0400

    Add shtmaker tool

---
+++ 1_ShtMaker/shtmaker.cpp
@@ -0,0 +1,54 @@
+
+
+#include	<stdio.h>
+
+
+
+
+
+
+// Read file stdin and emit C-code to print that content
+// line by line.
+//
+int main(int argc, char **argv)
+{
+	char	*line;
+	size_t	size = 0;
+
+	while( getline( &line, &size, stdin ) > 0 ) {
+
+		printf( "fprintf( f, \"" );
+
+		char	*s = line, c;
+
+		while( c = *s++ ) {
+
+			switch( c ) {
+
+				case '\n':
+					printf( "\\n\" );\n" );
+				break;
+
+				case '\t':
+					printf( "\\t" );
+				break;
+
+				case '"':
+					printf( "\\\"" );
+				break;
+
+				case '%':
+					printf( "%%" );
+				break;
+
+				default:
+					printf( "%c", c );
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
+
+
