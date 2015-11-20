a0abb7b7741f79ad144bcb2be47343f2ace557c8
billKarsh <karshb@janelia.hhmi.org>
2015-11-20 13:51:42 -0500

    Malloc -> vector

--- BK_Experiments/test.cpp
+++ BK_Experiments/test.cpp
@@ -12,10 +12,8 @@
 
 static void DrawImage()
 {
-	int		w = 1376, h = 1040, n = w * h;
-	uint8*	r = (uint8*)RasterAlloc( n );
-
-	memset( r, 0, n );
+	int				w = 1376, h = 1040, n = w * h;
+	vector<uint8>	r( n, 0 );
 
 	for( int y = 9; y <= h-9; y += 44 ) {
 		for( int x = 9; x <= w-9; x += 44 ) {
@@ -27,9 +25,7 @@ static void DrawImage()
 		}
 	}
 
-	Raster8ToTif8( "Lines.tif", r, w, h );
-
-	RasterFree( r );
+	Raster8ToTif8( "Lines.tif", &r[0], w, h );
 }
 
 
