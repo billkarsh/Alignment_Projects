e2c93ab827f97c81256ded179abf2e0010a73e03
billKarsh <karshb@janelia.hhmi.org>
2016-05-14 18:22:02 -0400

    Use defaults for missing TIFF tags where applicable

--- 0_GEN/ImageIO.cpp
+++ 0_GEN/ImageIO.cpp
@@ -189,7 +189,7 @@ static void Raster8FromTif16Bit(
 	uint16			fmt;
 
 // Check for signed data if 16-bit
-	TIFFGetField( tif, TIFFTAG_SAMPLEFORMAT, &fmt );
+	TIFFGetFieldDefaulted( tif, TIFFTAG_SAMPLEFORMAT, &fmt );
 
 // Read encoded strips
 	raw = (uint16*)malloc( npixels * sizeof(uint16) );
@@ -406,10 +406,18 @@ TIFFSetWarningHandler( oldEH );
 		exit( 42 );
 	}
 
-	TIFFGetField( tif, TIFFTAG_IMAGEWIDTH, &w );
-	TIFFGetField( tif, TIFFTAG_IMAGELENGTH, &h );
-	TIFFGetField( tif, TIFFTAG_BITSPERSAMPLE, &bps );
-	TIFFGetField( tif, TIFFTAG_SAMPLESPERPIXEL, &spp );
+	if( !TIFFGetField( tif, TIFFTAG_IMAGEWIDTH, &w ) ) {
+		fprintf( flog, "Raster8FromTif: Missing image width tag in [%s].\n", name );
+		exit( 42 );
+	}
+
+	if( !TIFFGetField( tif, TIFFTAG_IMAGELENGTH, &h ) ) {
+		fprintf( flog, "Raster8FromTif: Missing image length tag in [%s].\n", name );
+		exit( 42 );
+	}
+
+	TIFFGetFieldDefaulted( tif, TIFFTAG_BITSPERSAMPLE, &bps );
+	TIFFGetFieldDefaulted( tif, TIFFTAG_SAMPLESPERPIXEL, &spp );
 	npixels = w * h;
 
 	fprintf( flog,
@@ -489,11 +497,19 @@ TIFFSetWarningHandler( oldEH );
 		exit( 42 );
 	}
 
-	TIFFGetField( tif, TIFFTAG_IMAGEWIDTH, &w );
-	TIFFGetField( tif, TIFFTAG_IMAGELENGTH, &h );
-	TIFFGetField( tif, TIFFTAG_BITSPERSAMPLE, &bps );
-	TIFFGetField( tif, TIFFTAG_SAMPLESPERPIXEL, &spp );
-	TIFFGetField( tif, TIFFTAG_SAMPLEFORMAT, &fmt );
+	if( !TIFFGetField( tif, TIFFTAG_IMAGEWIDTH, &w ) ) {
+		fprintf( flog, "Raster16FromTif16: Missing image width tag in [%s].\n", name );
+		exit( 42 );
+	}
+
+	if( !TIFFGetField( tif, TIFFTAG_IMAGELENGTH, &h ) ) {
+		fprintf( flog, "Raster16FromTif16: Missing image length tag in [%s].\n", name );
+		exit( 42 );
+	}
+
+	TIFFGetFieldDefaulted( tif, TIFFTAG_BITSPERSAMPLE, &bps );
+	TIFFGetFieldDefaulted( tif, TIFFTAG_SAMPLESPERPIXEL, &spp );
+	TIFFGetFieldDefaulted( tif, TIFFTAG_SAMPLEFORMAT, &fmt );
 	npixels = w * h;
 
 	fprintf( flog,
